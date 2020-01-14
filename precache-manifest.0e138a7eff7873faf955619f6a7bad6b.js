cb20cb06bfff7f46d25179d6598af94f7720026d
oliver alejandro almaguer tostado <almaguero95@gmail.com>
2020-01-14 00:27:41 -0800

    Updates

---
+++ precache-manifest.0e138a7eff7873faf955619f6a7bad6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99b58045a2241032bcdb9f57e9896ab",
+    "url": "/moviereact/index.html"
+  },
+  {
+    "revision": "48f12451d2d95076a8c5",
+    "url": "/moviereact/static/css/main.72d883a4.chunk.css"
+  },
+  {
+    "revision": "d9cd638623a128391da8",
+    "url": "/moviereact/static/js/2.e349482b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/moviereact/static/js/2.e349482b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48f12451d2d95076a8c5",
+    "url": "/moviereact/static/js/main.e1975f27.chunk.js"
+  },
+  {
+    "revision": "63868a9f024665fb335d",
+    "url": "/moviereact/static/js/runtime-main.743ba416.js"
+  }
+]);
